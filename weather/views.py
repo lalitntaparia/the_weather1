c35bb849e5adc4a3b61de5df1a41f4ba2d58445d
lalitntaparia <lalitntaparia@gmail.com>
2018-10-20 22:15:26 +0530

    the_weather

---
+++ weather/views.py
@@ -0,0 +1,17 @@
+
+
+# Create your views here.
+
+#the_weather/weather/views.py
+
+from django.shortcuts import render
+import requests
+
+
+
+
+def index(request):
+	url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=20edd749e144676c0b6b8afa8ca10dce'
+    city = 'Las Vegas'
+    city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+    return render(request, 'weather/index.html') #returns the index.html template
